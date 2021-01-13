f2832f5f7c9e54a8cfcd21d41c18810ce8f7a348
Meiirzhan00 <54979320+Meiirzhan00@users.noreply.github.com>
2021-01-13 21:04:27 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,96 +1,50 @@
-class Solution:
-    def __init__(self,x: int) -> int:
-        self.x=x
-    def checking(self):
+class Solution:    
+    def reverse(self, x: int) -> int:
         s=''
-        while self.x>0:
-                n=self.x
+        if x==0:
+            s+='0'
+            
+        elif x<0 and str(x) not in '0' :
+            c=str(x).count('0')
+            s+='-'
+            x=-x
+            x//=10**c
+            while x>0:
+                n=x
                 n%=10
                 s+=str(n)
-                self.x//=10
-        return s
-    def reverse(self):
-        s=''
-        if self.x==0:
-            s+='0'
-        elif self.x<0 and str(self.x)[-1]=='0' :
-            s+='-'
-            self.x=-self.x
-            self.x//=10
-            self.checking()
+                x//=10
 
-        elif self.x<0 :
+        elif x<0 :
             s+='-'
-            self.x=-self.x
-            self.checking()
-        elif str(self.x)[-1]=='0':
-            self.x//=10
-            self.checking()
-            
+            x=-x
+            while x>0:
+                n=x
+                n%=10
+                s+=str(n)
+                x//=10
+
+        elif str(x) not in '0':
+            c=str(x).count('0')
+            x//=10**c
+            while x>0:
+                n=x
+                n%=10
+                s+=str(n)
+                x//=10
+
         else:
-            self.checking()
+             while x>0:
+                n=x
+                n%=10
+                s+=str(n)
+                x//=10
 
         return s
 
-p=Solution(x=123)
-print(p.reverse())
-
---------------------------------------
-
-def factorial(): 
-  n=int(input('Санды енгіз : ')) 
-  k=n 
-  x=1 
-  while n>0: 
-    x*=n 
-    n-=1 
-  print(f'{k} санының факториялы : {x}') 
-  Ans = [] 
-  d = 2 
-  while d <= x: 
-      if x % d == 0: 
-        Ans.append(d) 
-        x //= d 
-      else: 
-          d += 1 
-  q=1 
-  my_dict={} 
-  for item in range(1000): 
-    if item in Ans: 
-      my_dict[item]=Ans.count(item) 
-      q*=(item**(Ans.count(item)-1))*(item-1) 
-  print("Жәй сандарға жіктелген : ", Ans) 
-  print("Дәрежесі бойынша жіктелген : ", my_dict) 
-  print("Эйлер функциясының мәні : ", q) 
-def san(): 
-  x=int(input('Санды енгіз : ')) 
-  Ans = [] 
-  d = 2 
-  while d <= x: 
-      if x % d == 0: 
-        Ans.append(d) 
-        x //= d 
-      else: 
-          d += 1 
-  q=1 
-  my_dict={} 
-  for item in range(1000): 
-    if item in Ans: 
-      my_dict[item]=Ans.count(item) 
-      q*=(item**(Ans.count(item)-1))*(item-1) 
-  print("Жәй сандарға жіктелген : ", Ans) 
-  print("Дәрежесі бойынша жіктелген: ", my_dict) 
-  print("Эйлер функциясының мәні : ", q) 
-def tanda(): 
-  while True: 
-    print("Эйлер функциясын табу".center(20,'-')) 
-    st=input("Санның өзін табу үшін '1'-ді бас, факториалы бойынша табу үшін '2'-ні бас: ") 
-    if st=='1': 
-      san() 
-      break 
-    elif st=='2': 
-      factorial() 
-      break 
-    else: 
-      print('Қайта енгіз') 
-tanda()
+p=Solution()
+print(p.reverse(x=123))
+print(p.reverse(x=-123))
+print(p.reverse(x=120))
+print(p.reverse(x=0))
+            
