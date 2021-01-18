69f19dfac0a0cea0fa813ca955d918751b3bef4a
Meiirzhan00 <54979320+Meiirzhan00@users.noreply.github.com>
2021-01-18 21:14:38 +0600

    Create ReverseInteger.py

---
+++ ReverseInteger.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        result = 0
+        abs_x = abs(x)
+        while abs_x != 0 :
+            remainder = abs_x % 10 
+            abs_x = abs_x //10
+            
+            temp = result * 10 + remainder
+            result = temp
+        
+        if x < 0 :
+            result = -1 * result
+            
+        if result < - 2**31 or result > 2**31 -1:
+            result = 0
+        
+        return result
